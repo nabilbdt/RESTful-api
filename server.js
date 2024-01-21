f55a4884397865f9a9a11033aa01902336d18399
nabildev <nabilbayad969@gmail.com>
2024-01-21 19:36:45 +0100

    MONGOdb connection

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
+
+//MONGOdb connection
+mongoose.set("strictQuery", false);
+mongoose.
+connect('mongodb://127.0.0.1:27017/RESTfulApi')
+.then(() => {
+    console.log("MongoDB Connected") 
+    app.listen(3000, ()=>{
+        console.log("Server is running on port 3000");
+    }) 
+})
+.catch(err => console.log(err));
+
+
+
+
 
 //routes
 app.get('/', (req,res) => {
@@ -10,6 +27,3 @@ app.get('/blog', (req,res) => {
     res.send("Hello BLOG, I am NABIL  !");
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
-})
