54fbcc3e6acc4c6b7230a631896ac8747e5af09c
nabildev <nabilbayad969@gmail.com>
2024-01-22 12:31:32 +0100

    GET "ById"

--- server.js
+++ server.js
@@ -13,12 +13,25 @@ app.get('/', (req,res) => {
 app.get('/blog', (req,res) => {
     res.send("Hello BLOG, I am NABIL  !");
 })
+
+
+
 app.get('/products', async(req,res) => {
     try {
         const products = await Product.find({})
         res.status(200).json(products);
     } catch (error) {
         res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
 })
 
