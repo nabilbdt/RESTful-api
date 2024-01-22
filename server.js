894efd93ed36fbf2e5a6670d3204af371bdbdc83
nabildev <nabilbayad969@gmail.com>
2024-01-22 12:20:48 +0100

    GET request

--- server.js
+++ server.js
@@ -13,8 +13,16 @@ app.get('/', (req,res) => {
 app.get('/blog', (req,res) => {
     res.send("Hello BLOG, I am NABIL  !");
 })
+app.get('/products', async(req,res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
- app.post('/product', async(req,res) => {
+ app.post('/products', async(req,res) => {
     try {
       const product = await Product.create(req.body);
       res.status(200).json(product);
